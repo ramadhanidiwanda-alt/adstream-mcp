00477ce9906e9b15aff62b3e129a9ec1188eeed1
Ramadhani <ramadhanidiwanda@gmail.com>
2026-05-31 21:14:55 +0700

    feat: add ads broker rich contracts

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,16 @@ export { parseActionValue, parseActionValueFromValues } from './utils/parseActio
 export { formatCurrency, formatNumber } from './utils/formatCurrency.js';
 
 export type * from './types.js';
+export type * from './broker/types.js';
+export {
+  ADS_PROVIDER_IDS,
+  ADS_ENTITY_LEVELS,
+  ADS_OPERATION_KINDS,
+  ADS_TOOL_CATEGORIES,
+  isAdsProviderId,
+  isReadOperation,
+  defaultDenyWritePermissionPolicy,
+} from './broker/types.js';
 
 export { RuleEngine } from './rules/engine.js';
 export type * from './rules/types.js';
